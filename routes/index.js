8bc0b7f1409a514bc200774a7c407fe75954adbf
Vova Pihol <cetncat@gmail.com>
2017-05-19 23:40:07 +0300

    Add registration controller and form

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,19 @@ router.get('/', function(req, res, next) {
 
 router.get('/register', function (req, res) {
     res.render('register');
+});
+
+router.post('/register', function (req, res) {
+    var user = req.body;
+    models.User.create({
+        username: user.username,
+        password: user.password,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName
+    }).then(function () {
+        res.redirect('/');
+    })
 });
 
 module.exports = router;
